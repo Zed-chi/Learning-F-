ee4fe6a065d69e1e26c4bfc28e1baca45d47a2b2
AlexE <zed-chi@mail.ru>
2019-05-10 21:09:36 +0300

    Update ex_2.fs

--- ex_2.fs
+++ ex_2.fs
@@ -2,7 +2,4 @@ let g n = n + 5 // int
 
 let gg = fun n -> n + 5
 
-let square x = x * x
-let sumOfSquares (x, y) = square x + square y
-
-let h (x, y) = System.Math.Sqrt (sumOfSquares (x, y))
+let h (x, y) = System.Math.Sqrt ((x*x)+(y*y))
