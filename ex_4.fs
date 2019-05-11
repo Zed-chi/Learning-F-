a40ffc5a15f19f64c1748d56604d568377de738f
AlexE <zed-chi@mail.ru>
2019-05-11 17:46:18 +0300

    Update ex_4.fs

--- ex_4.fs
+++ ex_4.fs
@@ -1,17 +1,19 @@
-  // 7.1.1
-let rec fibo n = match n with
+// 7.1.1
+let rec fibo = function
     | 0 -> 0
     | 1 -> 1
+    | 2 -> 1
     | -1 -> 1
-    | n when n > 1 -> fibo (n-1) + fibo (n-2)
     | n when n < 0 -> fibo (n+2) - fibo (n+1)
+    | n -> fibo (n-1) + fibo (n-2)
 
 // 7.1.2
-let rec sum n = match n with
+let rec sum = function
     | n when n < 0 -> 0
     | 0 -> 0
     | 1 -> 1
     | n -> n + (sum (n-1))
+
 
 // 7.1.3
 let rec sum2 = function 
