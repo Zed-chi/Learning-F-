239b5ab5fd1a254e8d8b60fbdbc0bcc73c00e9c2
AlexE <zed-chi@mail.ru>
2019-05-31 14:58:22 +0300

    Update ex_11.fs

--- ex_11.fs
+++ ex_11.fs
@@ -4,18 +4,8 @@ type F =
 
 type TimeOfDay = { hours : int; minutes : int; f: F }
 
-let (.>.) x y = 
-    let compare = function
-        |(x, y) when x.f > y.f -> true
-        |(x, y) when x.f < y.f -> false
-        |(x, y) when x.hours > y.hours -> true
-        |(x, y) when x.hours < y.hours -> false
-        |(x, y) when x.minutes > y.minutes -> true
-        |_ -> false
-    compare (x,y)
-    
-(*
-let a = {hours = 12; minutes = 1; f= AM }
-let b = {hours = 1; minutes = 1; f= PM }
-printfn "%b" (b .>. a)
-*)
+let transform (x: TimeOfDay) = 
+    if x.f = PM then (x.hours+12) * 60 + x.minutes
+    else x.hours * 60 + x.minutes
+
+let (.>.) x y = (transform x) > (transform y) 
